6a81a0a84db5535b0253679aea6e8665643cc909
nick <nicholas.dietrich@gmail.com>
2013-05-14 22:28:55 -0500

    Changing name to sked

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-Garys::Application.configure do
+Sked::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # Code is not reloaded between requests
