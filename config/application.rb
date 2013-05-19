6366fc539bc4fad857c895dba87cbd51bcf87359
Nick Dietrich <nicholas.dietrich@gmail.com>
2013-05-18 19:18:18 -0500

    Attempt to fix timezone issue.

--- config/application.rb
+++ config/application.rb
@@ -27,7 +27,7 @@ module Sked
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    #config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'Central Time (US & Canada)'
     
     # Make ActiveRecord store dates in local time
     config.active_record.default_timezone = :local
